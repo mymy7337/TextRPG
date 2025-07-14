ca8342f1538dacd27050e0bc91e4f470cfbaf45a
kiub-15 <kmk923140@gmail.com>
2025-07-14 17:15:53 +0900

    GameManager 생성

---
+++ TextRPG/GameManager.cs
@@ -0,0 +1,10 @@
+﻿namespace GameManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
