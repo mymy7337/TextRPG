df31386a3d632805628a4afcd4ddbd5cd2a6997a
SeungYeop Oh <sadrain9274@naver.com>
2025-07-14 16:45:05 +0900

    [feat] 기본 능력치 필드 추가

--- TextRPG/Player.cs
+++ TextRPG/Player.cs
@@ -8,5 +8,15 @@ namespace TextRPG
 {
     internal class Player
     {
+        //player 기본상태
+        public int Level { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public int Atk { get; set; }
+        public int Def { get; set; }
+        public int Hp { get; set; }
+        public int Gold { get; private set; }
+        public int ExtraAtk { get; private set; } // 추가공격력
+        public int ExtraDef { get; private set; } // 추가방어력
     }
 }
