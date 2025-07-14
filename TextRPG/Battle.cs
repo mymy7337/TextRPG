b2af26990cebe8b79ac56fe581548d439eb3e370
mymy7337 <minyeong7337@gmail.com>
2025-07-14 17:17:34 +0900

    [Feat] 몬스터 소환 기능

--- TextRPG/Battle.cs
+++ TextRPG/Battle.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TextRPG
 {
     internal class Battle
     {
+        Random random = new Random();
+
         bool isWrong;
+        bool isMade;
         int choice;
         string message;
 
@@ -19,12 +24,18 @@ namespace TextRPG
 
         void StartUI()
         {
+            SpawnMonster();
             while (true)
             {
                 Console.Clear();
                 Console.WriteLine("Battle!!");
                 Console.WriteLine("");
-                //몬스터 정보
+
+                for (int i = 0; i < monsterSpanwed.Count; i++)
+                {
+                    Monster monster = monsterSpanwed[i];
+                    Console.WriteLine($"Lv.{monster.level:D2} {monster.name} HP {monster.hp}");
+                }
                 Console.WriteLine("");
                 Console.WriteLine("");
                 Console.WriteLine("[내정보]");
@@ -37,14 +48,14 @@ namespace TextRPG
                 Console.Write(">>");
                 isWrong = ChoiceCheck(1, 1);
 
-                switch(choice)
+                switch (choice)
                 {
                     case 1:
                         AttackUI();
                         break;
                 }
             }
-            
+
         }
 
         void AttackUI()
@@ -52,7 +63,11 @@ namespace TextRPG
             Console.Clear();
             Console.WriteLine("Battle!!");
             Console.WriteLine("");
-            //몬스터 정보
+            for (int i = 0; i < monsterSpanwed.Count; i++)
+            {
+                Monster monster = monsterSpanwed[i];
+                Console.WriteLine($"{i + 1} Lv.{monster.level:D2} {monster.name} HP {monster.hp}");
+            }
             Console.WriteLine("");
             Console.WriteLine("[내정보]");
             //플레이어 정보
@@ -63,9 +78,9 @@ namespace TextRPG
             Console.WriteLine(message);
             Console.Write(">>");
             isWrong = ChoiceCheck(0, 4);
-            switch(choice)
+            switch (choice)
             {
-                
+
             }
 
         }
@@ -85,21 +100,41 @@ namespace TextRPG
 
         }
 
+        struct Monster()
+        {
+            public int level;
+            public int hp;
+            public int atk;
+            public string name;
+        }
+        List<Monster> monsterData = new List<Monster>()
+        {
+            new Monster {level = 2, hp = 15, atk = 5, name = "미니언"},
+            new Monster {level = 3, hp = 10, atk = 9, name = "공허충"},
+            new Monster {level = 5, hp = 25, atk = 8, name = "대포미니언"},
+        };
+
+        List<Monster> monsterSpanwed = new List<Monster>();
 
         bool ChoiceCheck(int min, int max)
         {
             string input = Console.ReadLine();
-            if(int.TryParse(input, out choice))
+            if (int.TryParse(input, out choice))
             {
-                if(choice >= min && choice <= max)
-                return false;
+                if (choice >= min && choice <= max)
+                    return false;
             }
             return true;
         }
 
         void SpawnMonster()
         {
-
+            int monNum = random.Next(1, 5);
+            for (int i = 0; i < monNum; i++)
+            {
+                int monId = random.Next(0, 3);
+                monsterSpanwed.Add(monsterData[monId]);
+            }
         }
     }
 }
