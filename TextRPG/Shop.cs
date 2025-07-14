8c502c3bb98ee2db503f755d73c11f22f34cd761
Isillith <miungoat@gmail.com>
2025-07-14 18:03:59 +0900

    Create Shop.cs

---
+++ TextRPG/Shop.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TextRPG;
+
+public class Shop
+{
+    public void Shopping()
+    {
+        Console.Clear();
+        Console.WriteLine("상점에 입장하였다.");
+    }
+
+    
+    //장비랑 포션 리스트에서 랜덤으로 몇 개의 아이템 띄워 보여주기
+}
