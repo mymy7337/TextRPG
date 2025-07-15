963c9f6a3f69324df065a26a36e48eabdcaad9bb
Isillith <miungoat@gmail.com>
2025-07-15 15:15:53 +0900

    Update Shop.cs

--- TextRPG/Shop.cs
+++ TextRPG/Shop.cs
@@ -1,15 +1,38 @@
 using System;
+using System.Linq;
 
 namespace TextRPG;
 
 public class Shop
 {
-    public void Shopping()
+    public static void OpenShop()
     {
         Console.Clear();
-        Console.WriteLine("상점에 입장하였다.");
+        Console.WriteLine("상점에 입장하였습니다.");
+        Console.WriteLine("(E) 장비 / (P) 포션");
+        string input = Console.ReadLine().ToLower();
+
+        if (input == "p" || input == "buypotion")
+        {
+            ShowRandomPotions(3);
+        }
+        else if (input == "e" || input == "buyequipment")
+        {
+            ShowRandomEquipments(3);
+        }
+        else
+        {
+            Console.WriteLine("상점 주인: 뭘 찾는다고? 여기선 장비랑 포션밖에 안 팔아.");
+        }
     }
 
-    
-    //장비랑 포션 리스트에서 랜덤으로 몇 개의 아이템 띄워 보여주기
+    private static void ShowRandomPotions(int count)
+    {
+        var shopPotions = Potion.Items.Where(p => p.IType == Potion.ItemType.Shop).ToList();
+    }
+
+    private static void ShowRandomEquipments(int count)
+    {
+        var shopEquipments = Equipment.Items.Where(p => p.IType == Equipment.ItemType.Shop).ToList();
+    }
 }
