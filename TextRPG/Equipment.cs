937b01e261b78214787ebe7d22f47c0daf5b3c26
Isillith <miungoat@gmail.com>
2025-07-15 15:12:34 +0900

    Update Equipment.cs

--- TextRPG/Equipment.cs
+++ TextRPG/Equipment.cs
@@ -2,13 +2,10 @@ using System.Collections.Generic;
 
 namespace TextRPG;
 
-public class Equipment
+public class Equipment : Item
 {
-    public string ItemName { get; set; }
-    public string ItemScript { get; set; }
     public int ItemAttack { get; set; }
     public int ItemDefense { get; set; }
-    public int Price { get; set; } 
     public ItemType IType { get; set; }
 
     public enum ItemType
@@ -18,17 +15,16 @@ public class Equipment
     }
 
     public Equipment(string itemName, string itemScript, int itemAttack, int itemDefense, int price, ItemType iType)
+        : base(itemName, itemScript, price)
     {
-        ItemName = itemName;
-        ItemScript = itemScript;
         ItemAttack = itemAttack;
         ItemDefense = itemDefense;
-        Price = price;
         IType = iType;
     }
-    public static List<Equipment> equipments = new List<Equipment>
+
+    public static List<Equipment> Items = new List<Equipment>
     {
-        new Equipment("이름","어떤 아이템이다", 2, 3, 10, ItemType.Shop),
+        new Equipment("이름", "어떤 아이템이다", 2, 3, 10, ItemType.Shop),
         new Equipment("이름2", "어떤 아이템일까", 4, 5, 10, ItemType.Drop)
     };
 }
