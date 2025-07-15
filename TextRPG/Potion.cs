413800564e215bd47b78d127b33f31f48fb7ffde
Isillith <miungoat@gmail.com>
2025-07-15 15:12:39 +0900

    Update Potion.cs

--- TextRPG/Potion.cs
+++ TextRPG/Potion.cs
@@ -2,13 +2,10 @@ using System.Collections.Generic;
 
 namespace TextRPG;
 
-public class Potion
+public class Potion : Item
 {
-    public string ItemName { get; set; }
-    public string ItemScript { get; set; }
     public int ItemHP { get; set; }
     public int ItemMP { get; set; }
-    public int Price { get; set; } 
     public ItemType IType { get; set; }
 
     public enum ItemType
@@ -18,16 +15,15 @@ public class Potion
     }
 
     public Potion(string itemName, string itemScript, int itemHP, int itemMP, int price, ItemType iType)
+        : base(itemName, itemScript, price)
     {
-        ItemName = itemName;
-        ItemScript = itemScript;
         ItemHP = itemHP;
         ItemMP = itemMP;
-        Price = price;
         IType = iType;
     }
-    public static List<Potion> potions = new List<Potion>
+
+    public static List<Potion> Items = new List<Potion>
     {
-        new Potion("이름","어떤 아이템이다", 2, 3, 10, ItemType.Shop),
+        new Potion("이름", "어떤 아이템이다", 2, 3, 10, ItemType.Shop),
     };
 }
