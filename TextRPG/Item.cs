5b8b4840ad7c3bce2c3461ad7e104780f6e8ffbf
kiub-15 <kmk923140@gmail.com>
2025-07-16 17:43:26 +0900

    GameManager 네임스페이스 추가

--- TextRPG/Item.cs
+++ TextRPG/Item.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+namespace = TextRPG;
 public abstract class Item
 {
     public string ItemName { get; set; }
