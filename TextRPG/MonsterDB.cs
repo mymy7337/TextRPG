decf6aaa038168b01ae6937b93c9214f8907b1d3
mymy7337 <minyeong7337@gmail.com>
2025-07-17 21:12:01 +0900

    [Feat] 몬스터 드랍 추가

--- TextRPG/MonsterDB.cs
+++ TextRPG/MonsterDB.cs
@@ -10,9 +10,9 @@ namespace TextRPG
     {
         public static List<Monster> monsterData = new List<Monster>()
         {
-            new() {Level = 2, Hp = 15, MaxHp = 15, Atk = 5, Name = "미니언"},
-            new() {Level = 3, Hp = 10, MaxHp = 10, Atk = 9, Name = "공허충"},
-            new() {Level = 5, Hp = 25, MaxHp = 25, Atk = 8, Name = "대포미니언"},
+            new() {Level = 2, Hp = 15, MaxHp = 15, Atk = 5, Name = "미니언", Item = Equipment.Items[1]},
+            new() {Level = 3, Hp = 10, MaxHp = 10, Atk = 9, Name = "공허충", Item = Equipment.Items[1]},
+            new() {Level = 5, Hp = 25, MaxHp = 25, Atk = 8, Name = "대포미니언", Item = Equipment.Items[1]},
         };
     }
 }
