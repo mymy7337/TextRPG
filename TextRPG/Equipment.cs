b98d81e2e7e8cd9fc56744abdff582cce78feb26
Lee JaeEun <62467424+NextTheWAT@users.noreply.github.com>
2025-07-17 21:33:43 +0900

    zzz

--- TextRPG/Equipment.cs
+++ TextRPG/Equipment.cs
@@ -7,7 +7,7 @@ namespace TextRPG
         public int ItemAttack { get; set; }
         public int ItemDefense { get; set; }
 
-        public Equipment(string itemName, string itemScript, int gold, ItemType iType, int[] itemEffect, EffectType[] eTypes)
+        public Equipment(string itemName, string itemScript, int gold, GetType iType, int[] itemEffect, EffectType[] eTypes)
             : base(itemName, itemScript, gold, iType, itemEffect, eTypes)
         {
 
@@ -15,9 +15,9 @@ namespace TextRPG
 
         public static List<Equipment> Items = new List<Equipment>
     {
-        new Equipment("견습용 단검", "초보 모험가도 들기 쉬운 단검이다.", 2, ItemType.Shop, new int[]{3}, new EffectType[] { EffectType.ItemAtk } ),
-        new Equipment("고블린의 가죽갑옷", "적당히 몸에 맞게 수선해 입을 수 있을 것 같다. 몸통을 보호한다.", 5, ItemType.Drop, new int[]{5}, new EffectType[] { EffectType.ItemDef}),
-        new Equipment("무거운 판금갑옷", "높은 방어력을 제공하지만, 무겁다... 민첩하게 움직이긴 어려워진다.", 10, ItemType.Shop, new int[]{-2, 20}, new EffectType[] {EffectType.ItemAtk, EffectType.ItemDef})
+        new Equipment("견습용 단검", "초보 모험가도 들기 쉬운 단검이다.", 2, GetType.Shop, new int[]{3}, new EffectType[] { EffectType.ItemAtk } ),
+        new Equipment("고블린의 가죽갑옷", "적당히 몸에 맞게 수선해 입을 수 있을 것 같다. 몸통을 보호한다.", 5, GetType.Drop, new int[]{5}, new EffectType[] { EffectType.ItemDef}),
+        new Equipment("무거운 판금갑옷", "높은 방어력을 제공하지만, 무겁다... 민첩하게 움직이긴 어려워진다.", 10, GetType.Shop, new int[]{-2, 20}, new EffectType[] {EffectType.ItemAtk, EffectType.ItemDef})
     };
     }
 }
--- TextRPG/Item.cs
+++ TextRPG/Item.cs
@@ -4,67 +4,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TextRPG;
-public abstract class Item
+namespace TextRPG
 {
-    public string ItemName { get; set; }
-    public string ItemScript { get; set; }
-    public int Gold { get; set; }
-    public GetType IType { get; set; }
-
-    private int[] _itemEffect;
-    public int[] ItemEffect => _itemEffect;
-
-    private EffectType[] _effectTypes;
-    public EffectType[] EffectTypes => _effectTypes;
-
-    protected Item(string itemName, string itemScript, int gold, GetType iType, int[] itemEffect, EffectType[] effectTypes)
+    public abstract class Item
     {
-        ItemName = itemName;
-        ItemScript = itemScript;
-        Gold = gold;
-        IType = iType;
-        _itemEffect = itemEffect;
-        _effectTypes = effectTypes;
-    }
+        public string ItemName { get; set; }
+        public string ItemScript { get; set; }
+        public int Gold { get; set; }
+        public GetType IType { get; set; }
 
-    public enum GetType
-    {
-        Drop,
-        Shop,
-    }
+        private int[] _itemEffect;
+        public int[] ItemEffect => _itemEffect;
 
-    public enum EffectType
-    {
-        ItemAtk,
-        ItemDef,
-        ItemHp,
-        ItemMp
-    }
+        private EffectType[] _effectTypes;
+        public EffectType[] EffectTypes => _effectTypes;
 
-    public string ItemInfoText()
-    {
-        return $"{ItemName} | {ItemScript} | {Gold}G";
-    }
-
-    public virtual string ItemDetailsText()
-    {
-        var sb = new System.Text.StringBuilder();
-
-        sb.AppendLine($"이름: {ItemName}");
-        sb.AppendLine($"설명: {ItemScript}");
-        sb.AppendLine($"가격: {Gold} Gold");
-        sb.AppendLine($"획득방법: {IType}");
-
-        if (ItemEffect != null && EffectTypes != null)
+        protected Item(string itemName, string itemScript, int gold, GetType iType, int[] itemEffect, EffectType[] effectTypes)
         {
-            sb.AppendLine("효과:");
-            for (int i = 0; i < Math.Min(ItemEffect.Length, EffectTypes.Length); i++)
-            {
-                sb.AppendLine($" - {EffectTypes[i]}: +{ItemEffect[i]}");
-            }
+            ItemName = itemName;
+            ItemScript = itemScript;
+            Gold = gold;
+            IType = iType;
+            _itemEffect = itemEffect;
+            _effectTypes = effectTypes;
         }
-        return sb.ToString();
-    }
 
+        public enum GetType
+        {
+            Drop,
+            Shop,
+        }
+
+        public enum EffectType
+        {
+            ItemAtk,
+            ItemDef,
+            ItemHp,
+            ItemMp
+        }
+
+        public string ItemInfoText()
+        {
+            return $"{ItemName} | {ItemScript} | {Gold}G";
+        }
+
+        public virtual string ItemDetailsText()
+        {
+            var sb = new System.Text.StringBuilder();
+
+            sb.AppendLine($"이름: {ItemName}");
+            sb.AppendLine($"설명: {ItemScript}");
+            sb.AppendLine($"가격: {Gold} Gold");
+            sb.AppendLine($"획득방법: {IType}");
+
+            if (ItemEffect != null && EffectTypes != null)
+            {
+                sb.AppendLine("효과:");
+                for (int i = 0; i < Math.Min(ItemEffect.Length, EffectTypes.Length); i++)
+                {
+                    sb.AppendLine($" - {EffectTypes[i]}: +{ItemEffect[i]}");
+                }
+            }
+            return sb.ToString();
+        }
+
+    }
 }
