9171869dca1780d28e0f5abbb992ca8d026d7484
Isillith <miungoat@gmail.com>
2025-07-17 20:12:17 +0900

    Update Equipment.cs

--- TextRPG/Equipment.cs
+++ TextRPG/Equipment.cs
@@ -7,16 +7,16 @@ public class Equipment : Item
     public int ItemAttack { get; set; }
     public int ItemDefense { get; set; }
     
-    public Equipment(string itemName, string itemScript, int itemAttack, int itemDefense, int gold, ItemType iType)
-        : base(itemName, itemScript, gold, iType)
+    public Equipment(string itemName, string itemScript, int gold, ItemType iType, int[] itemEffect, EffectType[] eTypes)
+        : base(itemName, itemScript, gold, iType, itemEffect, eTypes)
     {
-        ItemAttack = itemAttack;
-        ItemDefense = itemDefense;
+       
     }
 
     public static List<Equipment> Items = new List<Equipment>
     {
-        new Equipment("이름", "어떤 아이템이다", 2, 3, 10, ItemType.Shop),
-        new Equipment("이름2", "어떤 아이템일까", 4, 5, 10, ItemType.Drop)
+        new Equipment("견습용 단검", "초보 모험가도 들기 쉬운 단검이다.", 2, ItemType.Shop, new int[]{3}, new EffectType[] { EffectType.ItemAtk } ),
+        new Equipment("고블린의 가죽갑옷", "적당히 몸에 맞게 수선해 입을 수 있을 것 같다. 몸통을 보호한다.", 5, ItemType.Drop, new int[]{5}, new EffectType[] { EffectType.ItemDef}),
+        new Equipment("무거운 판금갑옷", "높은 방어력을 제공하지만, 무겁다... 민첩하게 움직이긴 어려워진다.", 10, ItemType.Shop, new int[]{-2, 20}, new EffectType[] {EffectType.ItemAtk, EffectType.ItemDef})
     };
 }
