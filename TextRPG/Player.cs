ac045d8529715876b6e0e10852b25dd7033e6798
SeungYeop Oh <sadrain9274@naver.com>
2025-07-17 20:24:59 +0900

    [feat] 직업 enum 추가 및 주석화
    사용할때 주석 해제

--- TextRPG/Player.cs
+++ TextRPG/Player.cs
@@ -8,12 +8,21 @@ using System.Threading.Tasks;
 
 namespace TextRPG
 {
+    //public enum JobType // 직업 Enum
+    //{
+    //    Warrior,
+    //    Wizard,
+    //    Archer,
+    //    Thief,
+    //    Pirate
+    //}
     public class Player
     {
         //player 기본상태
         public int Level { get; set; }
         public string Name { get; set; }
         public string Job { get; set; }
+        //public JobType Job { get; set; }
         public int Atk { get; set; }
         public int Def { get; set; }
         //public int Dex { get; set; } // 민첩
