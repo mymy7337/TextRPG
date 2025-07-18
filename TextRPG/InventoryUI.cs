fcb6613779675d29716c5cdbf4bb3eede67d9280
SeungYeop Oh <sadrain9274@naver.com>
2025-07-18 12:58:25 +0900

    [refactory] 인벤토리 UI 메서드 이동

--- TextRPG/InventoryUI.cs
+++ TextRPG/InventoryUI.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace TextRPG
 {
-    internal class InventoryUI
+    public static class InventoryUI
     {
+        public static void DisplayInventory(Inventory inventory, bool showIdx) // 인벤토리 목록
+        {
+            Console.WriteLine("인벤토리");
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+            inventory.HasItemEquipped(showIdx);
+            Console.WriteLine();
+            Console.WriteLine("1. 장착 관리");
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine();
+        }
+
+        public static void DisplayEquip(Inventory inventory, bool showIdx) // 장착관리 목록
+        {
+            Console.WriteLine("인벤토리 - 장착 관리");
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+            inventory.HasItemEquipped(showIdx);
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine();
+        }
     }
 }
