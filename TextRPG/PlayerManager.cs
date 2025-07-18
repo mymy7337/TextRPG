dd88d38125feabe345155b054d0d11ea814fe3bf
SeungYeop Oh <sadrain9274@naver.com>
2025-07-18 09:51:59 +0900

    [feat] PlayerManager 클래스 생성 및 싱글톤

---
+++ TextRPG/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    internal class PlayerManager
+    {
+        public static PlayerManager instance;
+
+        public PlayerManager() 
+        { 
+            if (instance == null)
+                instance = this;
+        }
+    }
+}
+ 
